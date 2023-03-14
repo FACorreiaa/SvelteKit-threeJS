7dda51c25e8fc23341e4d4cf219185877d87d73d
fc <fc@gsmk.de>
2023-03-14 11:41:30 +0100

    add another red cube

--- src/lib/scene.js
+++ src/lib/scene.js
@@ -3,17 +3,20 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = 3;
 
 const geometry = new THREE.BoxGeometry();
 
 const material = new THREE.MeshStandardMaterial({
-	color: 0x00ff00,
-	metalness: 0.13
+	color: 'red',
+	metalness: 0.5
 });
 
 const cube = new THREE.Mesh(geometry, material);
+const cube2 = new THREE.Mesh(geometry, material);
+
 scene.add(cube);
+scene.add(cube2);
 
 const directionalLight = new THREE.DirectionalLight(0x9090aa);
 directionalLight.position.set(-10, 10, -10).normalize();
@@ -29,6 +32,8 @@ const animate = () => {
 	requestAnimationFrame(animate);
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
+	cube2.rotation.x += 0.5;
+	cube2.rotation.y += 0.5;
 	renderer.render(scene, camera);
 };
 
